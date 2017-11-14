94befd0200832fdffe06f0e80e12ed705c045ff1
Jordan Last <jordan.michael.last@gmail.com>
2017-11-14 13:57:18 -0700

    initial test works

--- function-plot-element.ts
+++ function-plot-element.ts
@@ -1,14 +1,33 @@
-import {html, render} from '../lit-html/lit-html';
+import {html} from '../lit-html/lit-html';
+import {render} from '../lit-html/lib/lit-extended';
 
-class FunctionPlotElement extends HTMLElement {
+declare var functionPlot: any;
+
+export class FunctionPlotElement extends HTMLElement {
+    _data: any;
+
+    set data(val) {
+        this._data = val;
+        this.render();
+    }
+
+    get data() {
+        return this._data;
+    }
+
     connectedCallback() {
         this.render();
     }
 
     render() {
         render(html`
-            <div>Hello there world</div>
+            <div id="graph"></div>
         `, this);
+
+        functionPlot({
+            target: '#graph',
+            data: this.data
+        });
     }
 }
 
---
+++ test/test-function-plot-element/test-function-plot-element.ts
@@ -0,0 +1,24 @@
+import {FunctionPlotElement} from '../../node_modules/function-plot-element/function-plot-element';
+import {html} from '../../node_modules/lit-html/lit-html';
+import {render} from '../../node_modules/lit-html/lib/lit-extended';
+
+FunctionPlotElement;
+
+class TestFunctionPlotElement extends HTMLElement {
+    data: any;
+
+    connectedCallback() {
+        this.data = [{
+            fn: 'x^4'
+        }];
+        this.render();
+    }
+
+    render() {
+        render(html`
+            <function-plot data=${this.data}></function-plot>
+        `, this);
+    }
+}
+
+window.customElements.define('test-function-plot-element', TestFunctionPlotElement);
